3491b8512b0de69ae432c23e1d9d15d772dcc945
wazza <wazza@step.finance>
2026-01-27 20:34:05 +0800

    fix password generator

--- rcs/profile.sh
+++ rcs/profile.sh
@@ -1,6 +1,6 @@
 alias cpkey='cat ~/.ssh/id_rsa.pub | pbcopy && echo "SSH Key Copied"'
 
-alias ez='code ~/.zshrc'
+alias ez='vi ~/.zshrc'
 alias sz='source ~/.zshrc'
 
 alias path='echo -e ${PATH//:/\\n}'
--- rcs/utils.sh
+++ rcs/utils.sh
@@ -8,5 +8,6 @@ alias pwd24='genpwd 24'
 # generate password including symbols with a function to allow for argument input
 genpwd() {
   local length=${1:-16}
-  openssl rand -base64 $((length * 3 / 4 + 1)) | tr -dc 'A-Za-z0-9!@#$%^&*()_+-=' | head -c "$length"
+  LC_ALL=C tr -dc 'A-Za-z0-9!@#$%^&*()_+=-' < /dev/urandom | head -c "$length"
+  echo
 }
